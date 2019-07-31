7fdaf9cb83917b4f1ac31685b5eef12ac30d738b
JoseChan <524233828@qq.com>
2019-07-31 21:31:51 +0800

    fix

---
+++ src/BaiduTongjiServiceProvider.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mushan
+ * Date: 2016/11/23
+ * Time: 16:57
+ */
+namespace JoseChan\BaiduTongji;
+
+use Illuminate\Support\ServiceProvider;
+use Mushan\BaiduTongji\BaiduTongji;
+
+class BaiduTongjiServiceProvider extends ServiceProvider
+{
+//    protected $defer = true;
+
+    public function boot()
+    {
+        $config_path = __DIR__ . '/config.php';
+        $this->publishes([
+            $config_path => config_path('baidu_tongji.php'),
+        ]);
+        $this->mergeConfigFrom(
+            $config_path, 'baidu_tongji'
+        );
+    }
+
+    public function register()
+    {
+        $this->app->singleton('BaiduTongji', function ($app) {
+            return new BaiduTongji(config('baidu_tongji'));
+        });
+    }
+}
